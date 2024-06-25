f1217fa42c8c9f7ec283f4843d2e5dac95beae89
jowisaidman <jsaidman@fi.uba.ar>
2024-06-25 12:07:04 -0300

    improvements

--- HowToFightRustBorrowChecker/README.md
+++
@@ -1,3 +0,0 @@
-# How to fight Rust's borrow checker... and win
-
-Examples from https://www.youtube.com/watch?v=Pg07HQJ0tvI
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,25 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect(); //In collections we need to specify the expected type
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing argumnents: {}", err);
+        process::exit(1);
+    });
 
-    let content = fs::read_to_string(config.file_name).expect(&format!("File not found: {}", config.file_name));
+    if let Err(e) = run(config) {
+        println!("Error while reading the file: {}", e);
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> { // Box<dyn Error> is basically to return any type of error
+    let content = fs::read_to_string(config.file_name)?;
 
     println!("The content of the file is: {}", content);
+
+    Ok(())
 }
 
 struct Config<'a> {
@@ -16,10 +28,14 @@ struct Config<'a> {
 }
 
 impl<'a> Config<'a> {    
-    fn new(args: &'a[String]) -> Config<'a> {
-        Config {
-            _query: &args[1],
-            file_name: &args[2],
+    fn new(args: &'a[String]) -> Result<Config<'a>, String> {
+        if args.len() < 3 {
+            Err("Not enough arguments".to_string())
+        } else {
+            Ok(Config {
+                _query: &args[1],
+                file_name: &args[2],
+            })
         }
     }
     
